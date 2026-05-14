e8f02d8b67dd8fed1cec1925c733aa45396dde72
Robert Wang <glorywong1001@gmail.com>
2026-05-15 05:41:19 +0800

    fix(config): add hint to virmrc

--- configs/vimrc.ts
+++ configs/vimrc.ts
@@ -1,7 +1,14 @@
-import { path } from 'zx'
+import { chalk, path } from 'zx'
 import { Config } from '../types/configs.d.ts'
+import tildify from 'tildify'
 
 export default {
   name: 'vimrc',
   getFilePath: ({ home }) => path.join(home, '.vimrc'),
+  hint: ({ filePath }) =>
+    chalk.yellow(
+      `You follow the instruction in the head of ${
+        tildify(filePath)
+      } to manually install Vim plugins`,
+    ),
 } satisfies Config
